a3018c27e0e3ea010111ffbeb3023e6ad25941a9
Israel Yance <israelyance@gmail.com>
2023-11-27 22:54:44 -0500

    add query parameters

--- main.py
+++ main.py
@@ -55,3 +55,7 @@ def get_movie(id: int):
         if item['id'] == id:
             return item
     return {}
+
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category: str, year: str):
+    return [movie for movie in movies if movie['category'] == category and movie['year'] == year]
